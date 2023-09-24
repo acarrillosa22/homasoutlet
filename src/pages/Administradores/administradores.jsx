d889e71ec6b3a8618556031fc92acad560db3cfa
Emanuelsalas <48725269+Emanuelsalas@users.noreply.github.com>
2023-09-24 16:29:57 -0600

    Update administradores.jsx
    merge solution

--- src/pages/Administradores/administradores.jsx
+++ src/pages/Administradores/administradores.jsx
@@ -1,11 +1,7 @@
-
 import React, { useState } from "react";
 import { useModal } from "../../hooks/useModal";
 import "./administradores.css";
 import ModalA from "../../components/modal/modal";
-import React from "react";
-import "./administradores.css"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {
   Table,
@@ -33,88 +29,6 @@ function Administradores() {
     <Container>
       <br />
       <Button color="success">
-  Container,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  FormGroup,
-  ModalFooter,
-} from "reactstrap";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faRightToBracket } from "@fortawesome/free-solid-svg-icons"; // Reemplaza con el ícono que quieras usar
-library.add(faRightToBracket); // Agrega el ícono a la biblioteca
-const data = [
-  { id: 1, personaje: "Naruto", anime: "Naruto" },
-  { id: 2, personaje: "Goku", anime: "Dragon Ball" },
-  { id: 3, personaje: "Kenshin Himura", anime: "Rurouni Kenshin" },
-  { id: 4, personaje: "Monkey D. Luffy", anime: "One Piece" },
-  {
-    id: 5,
-    personaje: "Edward Elric",
-    anime: "Fullmetal Alchemist: Brotherhood",
-  },
-  { id: 6, personaje: "Seto Kaiba", anime: "Yu-Gi-Oh!" },
-];
-function administradores() {
-  state = {
-    data: data,
-    form: {
-      id: "",
-      personaje: "",
-      anime: "",
-    },
-  };
-
-  editar = (dato) => {
-    var contador = 0;
-    var arreglo = this.state.data;
-    arreglo.map((registro) => {
-      if (dato.id == registro.id) {
-        arreglo[contador].personaje = dato.personaje;
-        arreglo[contador].anime = dato.anime;
-      }
-      contador++;
-    });
-    this.setState({ data: arreglo, modalActualizar: false });
-  };
-
-  eliminar = (dato) => {
-    var opcion = window.confirm(
-      "Estás Seguro que deseas Eliminar el elemento " + dato.id
-    );
-    if (opcion == true) {
-      var contador = 0;
-      var arreglo = this.state.data;
-      arreglo.map((registro) => {
-        if (dato.id == registro.id) {
-          arreglo.splice(contador, 1);
-        }
-        contador++;
-      });
-      this.setState({ data: arreglo, modalActualizar: false });
-    }
-  };
-
-  insertar = () => {
-    var valorNuevo = { ...this.state.form };
-    valorNuevo.id = this.state.data.length + 1;
-    var lista = this.state.data;
-    lista.push(valorNuevo);
-    this.setState({ modalInsertar: false, data: lista });
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      form: {
-        ...this.state.form,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
-  return (
-    <Container>
-      <br />
-      <Button color="success" >
         Crear
       </Button>
       <br />
@@ -128,12 +42,10 @@ function administradores() {
             <th>Coreo Electronico</th>
             <th>Rol</th>
             <th>Acciones</th>
-
           </tr>
         </thead>
 
         <tbody>
-
           {dataState.map((dato) => (
             <tr key={dato.cedula}>
               <td>{dato.cedula}</td>
@@ -149,30 +61,19 @@ function administradores() {
                 <FontAwesomeIcon icon={faSquareXmark} size="lg" />
                 </Button>
                 
-
-          {this.state.data.map((dato) => (
-            <tr key={dato.id}>
-              <td>{dato.id}</td>
-              <td>{dato.personaje}</td>
-              <td>{dato.anime}</td>
-              <td>
-                <Button
-                  color="primary"
-                  onClick={() => this.mostrarModalActualizar(dato)}
-                >
-                  Editar
-                </Button>{" "}
-                <Button color="danger" onClick={() => this.eliminar(dato)}>
-                  Eliminar
-                </Button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
+
       <ModalA isOpenA={isOpenActualizar} closeModal={closeModalActualizar} />
     </Container>
   );
+}
+
+export default Administradores;
+
 }
 
 export default Administradores;
